d4230b320767e3d8e447c8186641a69547aa4a53
Sean Madden <sean@seanmadden.net>
2012-11-30 20:55:32 -0500

    Working copy
    mend

---
+++ settings.py
@@ -0,0 +1,15 @@
+
+RTM_INBOX_EMAIL='xxxxxxxxx@rmilk.com'
+
+LOCAL_INBOX='yyyyy@zzzzzz.net'
+
+LOCAL_INBOX_PASSWORD='zyxxzpassword'
+LOCAL_INBOX_SERVER='imap.gmail.com'
+LOCAL_INBOX_PORT=993
+LOCAL_INBOX_USER='yyyyy@zzzzzz.net'
+LOCAL_SMTP_SERVER='smtp.gmail.com'
+LOCAL_SMTP_PORT=465
+
+ACCEPTED_EMAILS_LIST=(
+	'approvedsender@me.com', 
+	)
